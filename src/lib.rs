22d90de3b85ea96fb5a9ccacf038a7f105d66349
Fell-x27 <fell-x27@mail.ru>
2024-10-03 01:19:58 +0500

    shelley_addrs +;

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,18 @@
-use bech32::Bech32;
-use bech32::Hrp;
-use bs58;
 use pgrx::prelude::*;
-use bech32::{self, encode, decode};
 use serde_cbor::{to_vec, from_slice};
 use serde_json::Value;
 use pgrx::JsonB;
+
+use bech32::{self, Bech32, Hrp, encode, decode};
+use bs58;
+
 use blake2::{Blake2bVar};
 use blake2::digest::{VariableOutput, Update};
+
 use ed25519_dalek::{Signature, Signer, Verifier, SigningKey, VerifyingKey};
-use cardano_serialization_lib::{BaseAddress, EnterpriseAddress, RewardAddress, AddressKind, Credential, DeserializeError};
 
-::pgrx::pg_module_magic!();
 
-//bech32 +
-//base58 +
-//cbor +
-//blake2b +
-//ed25519 +
+pg_module_magic!();
 
 mod cardano {
     use super::*;
@@ -34,14 +29,13 @@ mod cardano {
             .expect("Failed to decode Base58 string")
     }
 
-    // Bech32 encode
+    // Bech32
     #[pg_extern]
     pub(crate) fn bech32_encode(hrp: &str, input: &[u8]) -> String {
         let hrp = Hrp::parse(hrp).expect("Failed to parse HRP");
         encode::<Bech32>(hrp, input).expect("Failed to encode Bech32")
     }
 
-    // Bech32 decode
     #[pg_extern]
     pub(crate) fn bech32_decode_prefix(bech32_str: &str) -> String {
         let (hrp, _) = decode(bech32_str).expect("Failed to decode Bech32 string");
@@ -58,20 +52,16 @@ mod cardano {
     #[pg_extern]
     pub(crate) fn jsonb_to_cbor(input: JsonB) -> Vec<u8> {
         let value: Value = serde_json::from_value(input.0).expect("Failed to parse JsonB");
-
-        // Сериализуем в CBOR
         to_vec(&value).expect("Failed to encode CBOR")
     }
 
     #[pg_extern]
     pub(crate) fn cbor_to_jsonb(cbor_bytes: &[u8]) -> JsonB {
         let value: Value = from_slice(cbor_bytes).expect("Failed to decode CBOR");
-
-        // Преобразуем обратно в JsonB
         JsonB(serde_json::to_value(&value).expect("Failed to convert to JsonB"))
     }
 
-    // B2B
+    // Blake2B
     #[pg_extern]
     pub(crate) fn blake2b(input: &[u8], output_length: i32) -> Vec<u8> {
         let output_length = output_length as usize;
@@ -90,7 +80,7 @@ mod cardano {
 
     // ed25519 sign
     #[pg_extern]
-    pub(crate) fn ed25519_sign_message(secret_key_bytes: Vec<u8>, message: &[u8]) -> Vec<u8> {
+    pub(crate) fn ed25519_sign_message(secret_key_bytes: &[u8], message: &[u8]) -> Vec<u8> {
         let signing_key = SigningKey::from_bytes(&secret_key_bytes.try_into().expect("Invalid secret key length"));
         let signature: Signature = signing_key.sign(message);
         signature.to_bytes().to_vec()
@@ -98,16 +88,18 @@ mod cardano {
 
     // ed25519 ver
     #[pg_extern]
-    pub(crate) fn ed25519_verify_signature(public_key_bytes: Vec<u8>, message: &[u8], signature_bytes: Vec<u8>) -> bool {
+    pub(crate) fn ed25519_verify_signature(public_key_bytes: &[u8], message: &[u8], signature_bytes: &[u8]) -> bool {
         let verifying_key = VerifyingKey::from_bytes(&public_key_bytes.try_into().expect("Invalid public key length"))
             .expect("Invalid public key");
         let signature = Signature::try_from(&signature_bytes[..]).expect("Invalid signature");
         verifying_key.verify(message, &signature).is_ok()
     }
 
+    //High-level tools
     pub mod tools {
-        use cardano_serialization_lib::{Address, JsError};
         use super::*;
+
+        // dRep view_id builders
         pub(crate) fn drep_id_encode_cip105(raw_id_bytes: &[u8], is_script: bool) -> String {
             let hrp = if is_script { "drep_script" } else { "drep" };
             bech32_encode(hrp, raw_id_bytes)
@@ -126,43 +118,121 @@ mod cardano {
             bech32_encode(hrp, &input_with_prefix)
         }
 
-        pub(crate) fn build_shelley_base_address(payment_cred: &[u8], stake_cred: &[u8], network: u8) -> String {
-            let p_cred = Credential::from_bytes(Vec::from(payment_cred)).unwrap();
-            let s_cred = Credential::from_bytes(Vec::from(stake_cred)).unwrap();
-            let addr_prefix = Some((if (network == 0) {"addr_test"} else {"addr"}).to_string());
-            BaseAddress::new(network, &p_cred, &s_cred).to_address().to_bech32(addr_prefix).unwrap()
+        // Shelley Addr builders
+        pub(crate) fn build_shelley_base_address(payment_cred: &[u8], stake_cred: &[u8], addr_type: u8, network_id: u8) -> String {
+            if addr_type > 5  {
+                panic!("Invalid addr_type: {}. Expected value <= 5.", addr_type);
+            }
+
+            if network_id > 1  {
+                panic!("Invalid network_id: {}. Expected value 0 or 1.", network_id);
+            }
+
+            if payment_cred.len() != 28 {
+                panic!("Invalid payment_cred length: {}. Expected 28 bytes.", payment_cred.len());
+            }
+
+            if stake_cred.len() != 28 {
+                panic!("Invalid stake_cred length: {}. Expected 28 bytes.", stake_cred.len());
+            }
+
+            let mut address_bytes = Vec::with_capacity(1 + payment_cred.len() + stake_cred.len());
+            let combined_byte = (addr_type << 4) | (network_id & 0x0F);
+            address_bytes.push(combined_byte);
+            address_bytes.extend_from_slice(payment_cred);
+            address_bytes.extend_from_slice(stake_cred);
+            let addr_prefix = if network_id == 0 { "addr_test" } else { "addr" };
+            bech32_encode(addr_prefix, &address_bytes)
         }
-        //
-        // pub(crate) fn build_shelley_enterprise_address(payment_cred: &[u8], network: u8) -> String {
-        //     let p_cred = Credential::from_bytes(Vec::from(payment_cred)).unwrap();
-        //     EnterpriseAddress::new(network, &p_cred).to_string()
-        // }
-        //
-        // pub(crate) fn build_shelley_reward_address(stake_cred: &[u8], network: u8) -> String {
-        //     let s_cred = Credential::from_bytes(Vec::from(stake_cred)).unwrap();
-        //     RewardAddress::new(network, &s_cred).to_string()
-        // }
-        //
-        // pub(crate) fn extract_shelley_base_addr_payment_cred(base_address_bech32: String) -> Vec<u8> {
-        //     let address = Address::from_bech32(&*base_address_bech32).unwrap();
-        //     let base_address = BaseAddress::from_address(&address).unwrap();
-        //     let credential = base_address.payment_cred();
-        //     credential.to_bytes()
-        // }
-        //
-        // pub(crate) fn extract_shelley_base_addr_stake_cred(base_address_bech32: String) -> Vec<u8> {
-        //     let address = Address::from_bech32(&*base_address_bech32).unwrap();
-        //     let base_address = BaseAddress::from_address(&address).unwrap();
-        //     let credential = base_address.stake_cred();
-        //     credential.to_bytes()
-        // }
-        //
-        // pub(crate) fn extract_shelley_enterprise_addr_payment_cred(base_address_bech32: String) -> Vec<u8> {
-        //     let address = Address::from_bech32(&*base_address_bech32).unwrap();
-        //     let base_address = EnterpriseAddress::from_address(&address).unwrap();
-        //     let credential = base_address.payment_cred();
-        //     credential.to_bytes()
-        // }
+
+        pub(crate) fn build_shelley_enterprise_address(payment_cred: &[u8], addr_type: u8, network_id: u8) -> String {
+            if addr_type < 6 || addr_type > 7 {
+                panic!("Invalid addr_type: {}. Expected value between 6 and 7.", addr_type);
+            }
+
+            if network_id > 1 {
+                panic!("Invalid network_id: {}. Expected value 0 or 1.", addr_type);
+            }
+
+            if payment_cred.len() != 28 {
+                panic!("Invalid payment_cred length: {}. Expected 28 bytes.", payment_cred.len());
+            }
+
+            let mut address_bytes = Vec::with_capacity(1 + payment_cred.len());
+            let combined_byte = (addr_type << 4) | (network_id & 0x0F);
+            address_bytes.push(combined_byte);
+            address_bytes.extend_from_slice(payment_cred);
+            let addr_prefix = if network_id == 0 { "addr_test" } else { "addr" };
+            bech32_encode(addr_prefix, &address_bytes)
+        }
+
+        pub(crate) fn build_shelley_reward_address(stake_cred: &[u8], addr_type: u8, network_id: u8) -> String {
+            if addr_type < 14 || addr_type > 15 {
+                panic!("Invalid addr_type: {}. Expected value between 14 and 15.", addr_type);
+            }
+
+            if network_id > 1 {
+                panic!("Invalid network_id: {}. Expected value 0 or 1.", addr_type);
+            }
+
+            if stake_cred.len() != 28 {
+                panic!("Invalid stake_cred length: {}. Expected 28 bytes.", stake_cred.len());
+            }
+
+            let mut address_bytes = Vec::with_capacity(1 + stake_cred.len());
+            let combined_byte = (addr_type << 4) | (network_id & 0x0F);
+            address_bytes.push(combined_byte);
+            address_bytes.extend_from_slice(stake_cred);
+            let addr_prefix = if network_id == 0 { "stake_test" } else { "stake" };
+            bech32_encode(addr_prefix, &address_bytes)
+        }
+
+        // Shelley Addr extractors
+        pub(crate) fn extract_shelley_addr_payment_cred(shelley_address_bech32: &str) -> Vec<u8> {
+            let raw_address = bech32_decode_data(&shelley_address_bech32);
+
+            if raw_address.len() < 29 {
+                panic!("Invalid address length: {}. Expected at least 29 bytes.", raw_address.len());
+            }
+
+            let payment_cred = raw_address[1..29].to_vec();
+            payment_cred
+        }
+        pub(crate) fn extract_base_addr_stake_cred(base_address_bech32: &str) -> Vec<u8> {
+            let raw_address = bech32_decode_data(&base_address_bech32);
+
+            if raw_address.len() != 57 {
+                panic!("Invalid address length: {} . Expected 57 bytes.", raw_address.len());
+            }
+
+            let stake_cred = raw_address[29..57].to_vec();
+            stake_cred
+        }
+
+        pub(crate) fn extract_reward_addr_stake_cred(reward_address_bech32: &str) -> Vec<u8> {
+            extract_shelley_addr_payment_cred(&reward_address_bech32)
+        }
+
+        // Shelley Addr type detector
+        pub(crate) fn get_addr_type(base_address_bech32: &str) -> String {
+            let raw_address = bech32_decode_data(&base_address_bech32);
+            let addr_type_byte = raw_address[0] >> 4;
+
+            let addr_type = match addr_type_byte {
+                0b0000 => "PMT_KEY:STK_KEY",
+                0b0001 => "PMT_SCRIPT:STK_KEY",
+                0b0010 => "PMT_KEY:STK_SCRIPT",
+                0b0011 => "PMT_SCRIPT:STK_SCRIPT",
+                0b0100 => "PMT_KEY:POINTER",
+                0b0101 => "PMT_SCRIPT:POINTER",
+                0b0110 => "PMT_KEY:NONE",
+                0b0111 => "PMT_SCRIPT:NONE",
+                0b1110 => "STK_KEY",
+                0b1111 => "STK_SCRIPT",
+                _ => "UNKNOWN",
+            };
+            addr_type.to_string()
+        }
     }
 }
 
@@ -228,7 +298,7 @@ mod tests {
             "science",
             "approach"
         ],
-        "version": 1.0_f64
+        "version": 1.0
     }));
 
         let expected_output = hex::decode("a3636164616b697320616d617a696e67216866656174757265738267736369656e636568617070726f6163686776657273696f6ef93c00")
@@ -246,7 +316,7 @@ mod tests {
             "science",
             "approach"
         ],
-        "version": 1.0_f64
+        "version": 1.0
     }));
 
         let cbor_bytes = hex::decode("a3636164616b697320616d617a696e67216866656174757265738267736369656e636568617070726f6163686776657273696f6ef93c00")
@@ -276,7 +346,7 @@ mod tests {
             .expect("Failed to decode hex");
         let expected_signature = hex::decode("74265F96E48EF1751F7C9CB3C5D376130664F6E00518FEFD10FB627112EF6DD29C424D335F236AECA9657B914FEC5DB9C0412E69858776B03A8FE476C0E7600F")
             .expect("Failed to decode hex");
-        let result_signature = crate::cardano::ed25519_sign_message(secret_key.clone(), message);
+        let result_signature = crate::cardano::ed25519_sign_message(&secret_key, message);
 
         assert_eq!(result_signature, expected_signature);
     }
@@ -289,7 +359,7 @@ mod tests {
         let signature = hex::decode("74265F96E48EF1751F7C9CB3C5D376130664F6E00518FEFD10FB627112EF6DD29C424D335F236AECA9657B914FEC5DB9C0412E69858776B03A8FE476C0E7600F")
             .expect("Failed to decode hex");
 
-        let is_valid = crate::cardano::ed25519_verify_signature(public_key.clone(), message, signature.clone());
+        let is_valid = crate::cardano::ed25519_verify_signature(&public_key, message, &signature);
 
         assert!(is_valid);
     }
@@ -331,16 +401,85 @@ mod tests {
     }
 
     #[pg_test]
-    fn test_build_shelley_base_address(){
+    fn test_build_shelley_base_address() {
         let p_cred = hex::decode("7415251fc7df0983fb1809b8b27e2d4578d8b7ca336be8656627e626").expect("Failed to decode hex");
         let s_cred = hex::decode("7c3ae2f2175c3d886b9daaa362533b7db1b30db6f2bafaed7569eeef").expect("Failed to decode hex");
-        let network = 1;
-        let base_address = crate::cardano::tools::build_shelley_base_address(&p_cred, &s_cred, network);
+        let addr_type = 0;
+        let network_id = 0;
+        let base_address = crate::cardano::tools::build_shelley_base_address(&p_cred, &s_cred, addr_type, network_id);
         let expected_result = "addr_test1qp6p2fglcl0snqlmrqym3vn794zh3k9hegekh6r9vcn7vfnu8t30y96u8kyxh8d25d39xwmakxesmdhjhtaw6atfamhsplwypm";
 
         assert_eq!(base_address, expected_result);
     }
 
+    #[pg_test]
+    fn test_build_shelley_enterprise_address() {
+        let p_cred = hex::decode("7415251fc7df0983fb1809b8b27e2d4578d8b7ca336be8656627e626").expect("Failed to decode hex");
+        let addr_type = 6;
+        let network_id = 0;
+        let base_address = crate::cardano::tools::build_shelley_enterprise_address(&p_cred, addr_type, network_id);
+        let expected_result = "addr_test1vp6p2fglcl0snqlmrqym3vn794zh3k9hegekh6r9vcn7vfspyp8fn";
+
+        assert_eq!(base_address, expected_result);
+    }
+
+    #[pg_test]
+    fn test_build_shelley_reward_address() {
+        let s_cred = hex::decode("7c3ae2f2175c3d886b9daaa362533b7db1b30db6f2bafaed7569eeef").expect("Failed to decode hex");
+        let addr_type = 14;
+        let network_id = 0;
+        let base_address = crate::cardano::tools::build_shelley_reward_address(&s_cred, addr_type, network_id);
+        let expected_result = "stake_test1up7r4chjzawrmzrtnk42xcjn8d7mrvcdkmet47hdw457amcl9yr85";
+
+        assert_eq!(base_address, expected_result);
+    }
+
+    #[pg_test]
+    fn test_extract_shelley_base_address_payment_cred() {
+        let base_address = "addr_test1qp6p2fglcl0snqlmrqym3vn794zh3k9hegekh6r9vcn7vfnu8t30y96u8kyxh8d25d39xwmakxesmdhjhtaw6atfamhsplwypm";
+        let expected_result = hex::decode("7415251fc7df0983fb1809b8b27e2d4578d8b7ca336be8656627e626").expect("Failed to decode hex");
+        let p_cred = crate::cardano::tools::extract_shelley_addr_payment_cred(&base_address);
+        assert_eq!(p_cred, expected_result);
+    }
+
+    #[pg_test]
+    fn test_extract_shelley_enterprise_address_payment_cred() {
+        let enterprise_address = "addr_test1vp6p2fglcl0snqlmrqym3vn794zh3k9hegekh6r9vcn7vfspyp8fn";
+        let expected_result = hex::decode("7415251fc7df0983fb1809b8b27e2d4578d8b7ca336be8656627e626").expect("Failed to decode hex");
+        let p_cred = crate::cardano::tools::extract_shelley_addr_payment_cred(&enterprise_address);
+        assert_eq!(p_cred, expected_result);
+    }
+
+    #[pg_test]
+    fn test_extract_base_address_stake_cred() {
+        let base_address = "addr_test1qp6p2fglcl0snqlmrqym3vn794zh3k9hegekh6r9vcn7vfnu8t30y96u8kyxh8d25d39xwmakxesmdhjhtaw6atfamhsplwypm";
+        let expected_result = hex::decode("7c3ae2f2175c3d886b9daaa362533b7db1b30db6f2bafaed7569eeef").expect("Failed to decode hex");
+        let s_cred = crate::cardano::tools::extract_base_addr_stake_cred(&base_address);
+        assert_eq!(s_cred, expected_result);
+    }
+
+    #[pg_test]
+    fn test_extract_reward_address_stake_cred() {
+        let reward_address = "stake_test1up7r4chjzawrmzrtnk42xcjn8d7mrvcdkmet47hdw457amcl9yr85";
+        let expected_result = hex::decode("7c3ae2f2175c3d886b9daaa362533b7db1b30db6f2bafaed7569eeef").expect("Failed to decode hex");
+        let s_cred = crate::cardano::tools::extract_reward_addr_stake_cred(&reward_address);
+        assert_eq!(s_cred, expected_result);
+    }
+
+    #[pg_test]
+    fn test_get_addr_type() {
+        let reward_address = "stake_test1up7r4chjzawrmzrtnk42xcjn8d7mrvcdkmet47hdw457amcl9yr85";
+        let reward_addr_type = crate::cardano::tools::get_addr_type(&reward_address);
+        assert_eq!(reward_addr_type, "STK_KEY");
+
+        let base_address = "addr_test1qp6p2fglcl0snqlmrqym3vn794zh3k9hegekh6r9vcn7vfnu8t30y96u8kyxh8d25d39xwmakxesmdhjhtaw6atfamhsplwypm";
+        let base_addr_type = crate::cardano::tools::get_addr_type(&base_address);
+        assert_eq!(base_addr_type, "PMT_KEY:STK_KEY");
+
+        let enterprise_address = "addr_test1vp6p2fglcl0snqlmrqym3vn794zh3k9hegekh6r9vcn7vfspyp8fn";
+        let enterprise_addr_type = crate::cardano::tools::get_addr_type(&enterprise_address);
+        assert_eq!(enterprise_addr_type, "PMT_KEY:NONE");
+    }
 }
 
 /// This module is required by `cargo pgrx test` invocations.
