8108164e6ced9ff9a90e2a7f7dd5dd0de237042e
Fell-x27 <fell-x27@mail.ru>
2025-02-23 03:34:02 +0500

    v1.0.5-p1 release
    Fixed version tag;

--- sql/pg_cardano--1.0.5--1.0.5-p1.sql
+++ sql/pg_cardano--1.0.5--1.0.5-p1.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION cardano.tools_verify_cip88_pool_key_registration(cbor_data BYTEA)
     RETURNS TABLE (is_valid BOOLEAN) AS $$
 BEGIN
-    RETURN QUERY
-        WITH
+RETURN QUERY
+    WITH
             decoded_jsonb AS (
                 SELECT "cardano"."cbor_decode_jsonb_hex2bytea"(cbor_data) AS json_data
             ),
@@ -16,27 +16,27 @@ BEGIN
                     (json_data#>>'{867, 2, 0, 2, 3}')::"bytea" AS signature
                 FROM decoded_jsonb
             )
-        SELECT
-            cardano.ed25519_verify_signature(
-                    pubkey,
-                    message,
-                    signature
-            ) AND ("address" = "expected_address") AS is_valid
-        FROM parsed_fields, LATERAL (
-            SELECT
-                "cardano"."cbor_encode_jsonb"(
-                        jsonb_build_array(
-                                'Signature1',
-                                protected_header,
-                                ''::bytea,
-                                CASE
-                                    WHEN need_hash = 1 THEN cardano.blake2b_hash(payload, 28)
-                                    ELSE payload
-                                    END
-                        )
-                ) AS message,
-                "cardano".blake2b_hash("pubkey", 28) AS expected_address
-            ) subquery;
+SELECT
+    cardano.ed25519_verify_signature(
+            pubkey,
+            message,
+            signature
+    ) AND ("address" = "expected_address") AS is_valid
+FROM parsed_fields, LATERAL (
+                             SELECT
+                                 "cardano"."cbor_encode_jsonb"(
+                                         jsonb_build_array(
+                                                 'Signature1',
+                                                 protected_header,
+                                                 ''::bytea,
+                                                 CASE
+                                                     WHEN need_hash = 1 THEN cardano.blake2b_hash(payload, 28)
+                                                     ELSE payload
+                                                     END
+                                         )
+                                 ) AS message,
+                                 "cardano".blake2b_hash("pubkey", 28) AS expected_address
+    ) subquery;
 END;
 $$ LANGUAGE plpgsql;
 
